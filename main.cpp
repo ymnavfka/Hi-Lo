792c7116a718b23c91311e09609236cf733dad5f
ymnavfka <ymnavfka@mail.ru>
2021-12-13 10:00:21 +0300

    add random genegation of number

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
+#include <random>
+
 int main()
 {
-    
+   std::cout<<"Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n";
+   std::random_device rd; 
+   std::mt19937 mersenne(rd());
+   auto randomNum{mersenne() % 99 + 1};
+   std::cout << randomNum << '\n';
+   
    return 0;
 }
