2d70f4de7f45dd90c38460034399331b23a87a84
ymnavfka <ymnavfka@mail.ru>
2021-12-13 10:46:53 +0300

    finished program

--- main.cpp
+++ main.cpp
@@ -17,14 +17,65 @@ int getUserGuess()
 
 int main()
 {
-   std::cout<<"Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n";
-   std::random_device rd; 
-   std::mt19937 mersenne(rd());
-   auto randomNum{mersenne() % 99 + 1};
-   std::cout << randomNum << '\n';
+   int tryCounter{ 1 };
 
-   int userGuess{ getUserGuess() };
-   std::cout << userGuess;
+   while (true)
+   {
+      std::cout<<"Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n";
+      std::random_device rd; 
+      std::mt19937 mersenne(rd());
+      auto randomNum{mersenne() % 99 + 1};
+
+      int userGuess{ getUserGuess() };
+
+      if (userGuess != randomNum)
+      {
+         if (userGuess < randomNum)
+         {
+            std::cout << "Your guess is too low.\n";
+         }
+         else if (userGuess > randomNum)
+         {
+            std::cout << "Your guess is too high.\n";
+         }
+         ++tryCounter;
+      }
+      else
+      {
+         if (tryCounter <= 7)
+         {
+            std::cout << "Correct! You win!\n";
+         }
+         else
+         {
+            std::cout << "Sorry, you lose. The correct number was " << randomNum << ".\n";
+         }
+         
+         bool isUserInputCorrect{};
+         do
+         {
+            std::cout << "Would you like to play again (y/n)? ";
+            char userAnswer{};
+            std::cin >> userAnswer;
+            if (userAnswer == 'y')
+            {
+               tryCounter = 1;
+               isUserInputCorrect = true;
+               continue;
+            }
+            else if (userAnswer == 'n')
+            {
+               std::cout << "Thank you for playing.\n";
+               isUserInputCorrect = true;
+               break;
+            }
+            else
+            {
+               isUserInputCorrect = false;
+            }
+         } while (!isUserInputCorrect);
+      }
+   }
 
    return 0;
 }
