c73de2a3f1b960542ace9452072aeb507d0566af
ymnavfka <ymnavfka@mail.ru>
2021-12-13 10:08:39 +0300

    add getUserGuess func and use it in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include <random>
 
+int getUserGuess()
+{
+    while(true)
+    {
+       static int guessNum{ 1 };
+       std::cout << "Guess #" << guessNum <<": ";
+       int userNum{};
+       std::cin >> userNum;
+       std::cin.ignore(pow(2,16), '\n');
+       if (userNum >= 1 && userNum <= 100)
+            return userNum;
+    }
+}
+
 int main()
 {
    std::cout<<"Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is.\n";
@@ -8,6 +22,9 @@ int main()
    std::mt19937 mersenne(rd());
    auto randomNum{mersenne() % 99 + 1};
    std::cout << randomNum << '\n';
-   
+
+   int userGuess{ getUserGuess() };
+   std::cout << userGuess;
+
    return 0;
 }
